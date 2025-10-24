3064de678e54fe6e65997c8c5149354cb0153ac8
baranekumar56 <baranekumar56@gmail.com>
2025-10-24 12:19:09 +0530

    added db

---
+++ app/database.py
@@ -0,0 +1,16 @@
+
+from motor.motor_asyncio import AsyncIOMotorClient
+
+mongourl = ""
+
+engine = AsyncIOMotorClient(mongourl)
+
+database = engine.get_database('notes')
+
+# collections
+workspaces = database.get_collection('workspace')
+users = database.get_collection('users')
+notes = database.get_collection('notes')
+contents = database.get_collection('content') 
+collabs = database.get_collection('collabs')
+
--- app/schema/note.py
+++ app/schema/note.py
@@ -7,5 +7,21 @@ class NoteCreate(BaseModel):
     user_id : str
     workspace_id : str
     created_at : datetime = Field(default_factory=datetime.now)
+    header : str
 
+class Note(NoteCreate):
+
+    note_id : str
+
+
+class ContentCreate(BaseModel):
+
+    section_no : int
+    note_id : str
+    content : str
+    created_at : datetime = Field(default_factory=datetime.now)
+
+class Content(ContentCreate):
     
+    content_id : str
+
