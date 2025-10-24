e1c6807ea1f2aa80174cdd68f2ea27ccbd9d8276
sylendra807 <vinayaksylendra@gmail.com>
2025-10-24 13:13:07 +0530

    main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
 from fastapi import FastAPI, APIRouter
-
+from app.routes import note as note_rouer, user_routes, workspace as workspace_router
 
 app = FastAPI()
 
+app.include_router(user_routes.router)
+app.include_router(workspace_router.router) 
+app.include_router(note_rouer.router)
 @app.get("/")
 def root():
-
     return {"msg": "everything seems working fine"}
-
